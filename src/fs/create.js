b3a7961ca817f71ca526a83cdb0b3422106be3dd
annTerry <annak@1gb.ru>
2022-12-01 19:34:06 +0300

    feat: fs/create.js ready

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,28 @@
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const create = async () => {
-    // Write your code here 
+    const baseFile = path.join(__dirname, 'files', 'fresh.txt');
+    try {
+      await fs.access(baseFile);
+      throw new Error("FS operation failed!");
+    } catch (err) {
+        if (err.syscall && err.syscall === 'access') {
+          const data = new Uint8Array(Buffer.from('I am fresh and young'));
+          await fs.writeFile(baseFile, data, (err) => {
+            if (err) throw err;
+            console.log('The file has been saved!');
+          });
+        }
+        else {
+          console.log (err.message);
+        }
+    }
 };
 
 await create();
