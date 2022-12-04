c8a7241f793f4e025b228dae0fda6223bc23674a
annTerry <annak@1gb.ru>
2022-12-04 18:52:17 +0300

    feat: fs/delete.js done

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const remove = async () => {
-    // Write your code here 
+  const baseFile = path.join(__dirname, 'files', 'fileToRemove.txt');
+  try {
+    await fs.access(baseFile);
+    await fs.unlink(baseFile, () => {
+      console.log("Successfully deleted file!");
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 await remove();
