6bc5e11026ac1dd735217b51b094733a3f0bb1d8
annTerry <annak@1gb.ru>
2022-12-05 01:32:25 +0300

    feat: zip/compress.js done

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,19 @@
+import process from 'node:process';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const transform = async () => {
-    // Write your code here 
-};
+  
+  process.stdin.on("data", data => { 
+    
+    }); 
+    process.on("exit", () => { 
+    writeStream.close();  
+    }); 
+  };
 
 await transform();
--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+import zlib from 'node:zlib';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const compress = async () => {
-    // Write your code here 
+  const gzip = zlib.createGzip(); 
+  const inp = fs.createReadStream(path.join(__dirname,'files', 'fileToCompress.txt')); 
+  const out = fs.createWriteStream(path.join(__dirname,'files', 'archive.gz')); 
+  inp.pipe(gzip).pipe(out);  
 };
 
 await compress();
