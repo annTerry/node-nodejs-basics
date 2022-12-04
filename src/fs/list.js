9e58d084ecd314fd38c8e039d2664981f4ae7ab6
annTerry <annak@1gb.ru>
2022-12-04 19:33:35 +0300

    feat: fs/list.js done

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,36 @@
+import fs from 'node:fs/promises';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const getFilesFromDirectory = async (dir) => {
+  const filesInDir = await fs.readdir(dir);
+  const files = await Promise.all(
+    filesInDir.map(async (file) => {
+          const filePath = path.join(dir, file);
+          const stats = await fs.stat(filePath);         
+          if (stats.isDirectory()) {
+              return getFilesFromDirectory(filePath);
+          } else {
+              return filePath;
+          }
+      })
+  );
+  return files; // return with empty arrays removed
+};
+
 const list = async () => {
-    // Write your code here 
+  const filesPath = path.join(__dirname, 'files');
+  try {
+    await fs.access(filesPath);
+    console.log(await getFilesFromDirectory(filesPath));
+    }
+    catch (err) {
+      console.log(err);
+    }
 };
 
 await list();
