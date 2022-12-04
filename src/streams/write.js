32da63873d69b22b37a0c8808c794da37c444f90
annTerry <annak@1gb.ru>
2022-12-05 01:08:35 +0300

    feat: streams/write.js done

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import process from 'node:process';
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const write = async () => {
-    // Write your code here 
+    const writeStream = fs.createWriteStream(path.join(__dirname,'files', 'fileToWrite.txt'));
+    process.stdin.on("data", data => { 
+      writeStream.write(data); 
+      }); 
+      process.on("exit", () => { 
+      writeStream.close();  
+      }); 
 };
 
 await write();
