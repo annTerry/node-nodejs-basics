e8ba77c4422ceea6bc72c40a6c2f6062195a0e28
annTerry <annak@1gb.ru>
2022-12-05 00:50:34 +0300

    feat: streams/read.js done

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import fs from 'node:fs';
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const read = async () => {
-    // Write your code here 
+  const stream = fs.ReadStream(path.join(__dirname,'files', 'fileToRead.txt'), {encoding: 'utf-8'});
+  stream.on('readable', function(){
+    var data = stream.read();
+    if(data != null) console.log(data);
+  });
 };
 
 await read();
