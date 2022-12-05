d2bb8ad422202a8c00f6cb6a3a19a099e8b8e76b
annTerry <annak@1gb.ru>
2022-12-05 16:51:34 +0300

    feat: wt done

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,30 @@
+import { Worker } from 'node:worker_threads';
+import os from 'node:os'
+import { fileURLToPath } from 'node:url';
+import path from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function runService(workerData) {
+  return new Promise((resolve, reject) => {
+    const worker = new Worker(path.join(__dirname, '/worker.js'), { workerData });
+    worker.on('message', resolve);
+    worker.on('error', (reject) => {return {status: 'error', data: null}});
+    worker.on('exit', (code) => {
+      if (code !== 0)
+        reject(new Error(`Worker stopped with exit code ${code}`));
+    })
+  })
+}
+
 const performCalculations = async () => {
-    // Write your code here
+  const results = [];
+  for(let i=0; i<os.cpus().length; i++) {
+    results.push(runService(10 + i));
+  }
+  const result = await Promise.all(results);
+  console.log(result);
 };
 
 await performCalculations();
