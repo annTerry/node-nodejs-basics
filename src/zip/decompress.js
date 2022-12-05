9f24a3532f4c975593f952aa08ae4489ee4c472c
Anna <89348633+annTerry@users.noreply.github.com>
2022-12-05 21:49:56 +0300

    fix: some minor fixes in zip

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -3,15 +3,15 @@ import zlib from 'node:zlib';
 import { fileURLToPath } from 'node:url';
 import path from 'node:path';
 
-
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const decompress = async () => {
   const unzip = zlib.createUnzip();
-  const inp = fs.createReadStream(path.join(__dirname,'files', 'archive.gz')); 
-  const out = fs.createWriteStream(path.join(__dirname,'files', 'fileToCompress.txt')); 
-  inp.pipe(unzip).pipe(out); 
+  const archive = fs.createReadStream(path.join(__dirname,'files', 'archive.gz')); 
+  const outputFile = fs.createWriteStream(path.join(__dirname,'files', 'fileToCompress.txt')); 
+  archive.pipe(unzip).pipe(outputFile);
+  console.log('File successfully decompressed!');
 };
 
 await decompress();
