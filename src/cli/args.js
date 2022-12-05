62f96f68c668c03f336718652e96d8decaf52feb
Anna <89348633+annTerry@users.noreply.github.com>
2022-12-05 20:59:03 +0300

    fix: cli/args.js fix output

--- src/cli/args.js
+++ src/cli/args.js
@@ -4,7 +4,7 @@ const parseArgs = () => {
   let key = '';
   const result = [];
   for (const argument of process.argv) {
-    if (key ==='' && argument.indexOf('--') === 0) key = argument;
+    if (key ==='' && argument.indexOf('--') === 0) key = argument.substring(2);
     else {
       if (key !=='') result.push(`${key} is ${argument}`);
       key = '';
