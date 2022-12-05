e5a4f5b154f0c88c456e0cf95daf3ca80c5e516e
Anna <89348633+annTerry@users.noreply.github.com>
2022-12-05 19:47:43 +0300

    fix: fs/copy.js throw error fixes

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -9,45 +9,47 @@ const __dirname = path.dirname(__filename);
 const copyFilesFromDirectory = async (oldDirPath, newDirPath) => {
   try {
     await fs.access(newDirPath);
+  }
+  catch {
+    await fs.mkdir(newDirPath);
+  }
+  const filesInDirectory = await fs.readdir(oldDirPath);
+  filesInDirectory.forEach(async (file) => {
+    const oldFilePath = path.join(oldDirPath, file);
+    const newFilePath = path.join(newDirPath, file);
+    const stats = await fs.stat(oldFilePath);
+    if (stats.isDirectory()) {
+      await copyFilesFromDirectory(oldFilePath, newFilePath);
+    } else {
+      try {
+        await fs.copyFile(oldFilePath, newFilePath);
+      } catch (error) {
+        console.log(error);
+      }
     }
-    catch {
-      await fs.mkdir( newDirPath );
-    }
-    const filesInDirectory = await fs.readdir(oldDirPath);
-    filesInDirectory.forEach(async (file) => {
-          const oldFilePath = path.join(oldDirPath, file);
-          const newFilePath = path.join(newDirPath, file);
-          const stats = await fs.stat(oldFilePath);          
-          if (stats.isDirectory()) {
-              await copyFilesFromDirectory(oldFilePath, newFilePath);
-          } else {
-            try {
-              await fs.copyFile(oldFilePath, newFilePath);
-              console.log('copied successfully!');
-            } catch (error) {
-              console.log(error);
-            }
-          }
-      })
+  })
 }
 
 
 const copy = async () => {
-const oldDirPath = path.join(__dirname, 'files');
-const newDirPath = path.join(__dirname, 'files_copy');
-try {
-await fs.access(oldDirPath)
-}
-catch (err) {
-  console.log(err);
-}
-try {
-  await fs.access(newDirPath);
+  const oldDirPath = path.join(__dirname, 'files');
+  const newDirPath = path.join(__dirname, 'files_copy');
+  try {
+    await fs.access(oldDirPath);
+    try {
+      await fs.access(newDirPath);
+      throw new Error("FS operation failed!");
+    }
+    catch {
+      await fs.mkdir(newDirPath);
+      await copyFilesFromDirectory(oldDirPath, newDirPath);
+      console.log("Files copied!");
+    }
   }
-  catch {
-    await fs.mkdir( newDirPath );
-    await copyFilesFromDirectory(oldDirPath, newDirPath);
+  catch (err) {
+    throw new Error("FS operation failed!");
   }
+
 };
 
 copy();
